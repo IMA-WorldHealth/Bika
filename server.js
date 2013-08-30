039b2cb5b73bf83fb8cfa846962bb04168ab5c62
Jonathan Niles <jonathanwniles@gmail.com>
2013-08-30 11:36:00 +0100

    authentification added; expected database is bika; a temporary mysql connector added;

--- server.js
+++ server.js
@@ -3,18 +3,24 @@
 var express = require('express')
   , app = express();
 
-app.set('env', 'development'); // Change this to change application behavior
+app.set('env', 'jonathan'); // Change this to change application behavior
 
-app.configure('development', function () {
+app.configure('dedrick', function () {
   app.use(express.bodyParser());
 });
 
-app.configure('login', function () {
+app.configure('jonathan', function () {
+  app.use(express.cookieParser());
+  app.use(express.session({secret: 'open blowfish'}));
   app.use(express.bodyParser());
   var auth = require('./lib/auth');
   app.use(auth);
 });
 
-app.use(express.static('static'));
 
-app.listen(3000, console.log("Rapid Prototype listening on port 3000"));
+// using main.html until dedrick and I sync our repositories
+app.get('/public/main.html', function (req, res, next) {
+  res.sendfile('./public/main.html')
+});
+
+app.listen(3000, console.log(app.get('env'), "Rapid Prototype listening on port 3000"));
