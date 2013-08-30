4c9c59bfd03cd451c2a5a3db82f6fee226d7aff9
dedrick <kitamuka@gmail.com>
2013-08-30 16:29:29 +0100

    dbFace.js at lib/database

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var dbFace = require('./lib/database/dbFace');
 var queryHandler = require('./lib/database/myQueryHandler');
 var path = require('path');
 var url = require('url');
-var express = require('express')
-  , app = express();
+var express = require('express'), app = express();
 
 app.set('env', 'dbface'); // Change this to change application behavior
 
@@ -18,31 +17,20 @@ app.configure('login', function () {
   app.use(auth);
 });
 
-app.post('/login', function (req, res) {
-	buidJSON(req.body);
-	console.log(req.body);
-
-
-});
-
-app.get('/data', function (req, res) {
-	 var cb = function (err, ans) {
+app.get('/data/:table', function (req, res) {
+  var cb = function (err, ans) {
       if (err) {
         throw err;
       } else {
-        res.setHeader('Content-Range', '0-0/' + ans.length);
+       // res.setHeader('Content-Range', '0-0/' + ans.length);
         res.json(ans);
       }
     };
- var myRequest = decodeURIComponent(url.parse(req.url).query); 
- var jsRequest = JSON.parse(myRequest);
- var Qo = queryHandler.getQueryObj(jsRequest);
- console.log('la requette a executee est :', Qo);
-	//df.selectionner(Qo, cb);
-	res.send();
-
+  var myRequest = decodeURIComponent(url.parse(req.url).query);
+  var jsRequest = JSON.parse(myRequest);
+  var Qo = queryHandler.getQueryObj(jsRequest);
+  dbFace.selectionner(Qo, cb);
 });
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, console.log("Rapid Prototype listening on port 3000"));
