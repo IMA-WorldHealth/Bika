91a8675050a020ce5bdc12aa47f06e763e24b14e
dedrick <kitamuka@gmail.com>
2013-09-05 10:17:39 +0100

    finalisation indexDedrick.html

--- public/js/dojoos/customizedModule/SelectX.js
+++ public/js/dojoos/customizedModule/SelectX.js
@@ -1,4 +1,4 @@
-define('customizedModule/SelectX', ["dojo/_base/declare", "dojo/on", "dojo/topic", "dijit/form/Select", "dojo/aspect", "dojo/_base/lang"], function(declare, on, topic, Select, aspect, lang) {
+define('customizedModule/SelectX', ["dojo/_base/declare", "dojo/on", "dojo/topic", "dijit/form/Select", "dojo/aspect", "dojo/_base/lang"], function (declare, on, topic, Select, aspect, lang) {
   return declare("customizedModule.SelectX", [Select], {
 
     // SelectX attempts to empower dijit/form/Select with asynchronous loading
@@ -10,51 +10,47 @@ define('customizedModule/SelectX', ["dojo/_base/declare", "dojo/on", "dojo/topic
     // Expects a dijit, type of event, and callback
     // E.g. selectx.detect(selectx2, 'onChange', function(val) {selectx.reset(val)});
     // This function can be very helpful in changing several selects together.
-    detect: function(widget, evt, callback) {
+    detect: function (widget, evt, callback) {
       var channel = widget.get('id') + evt;
       var h = topic.subscribe(channel, callback);
       // push this to the event listeners to unsubscribe later
       this._listeners.push(h);
     },
 
-    _emit: function(e) {
+    _emit: function (e) {
       // API: will emit `event`, `newval`
       var resultValue = {};
       resultValue.value = this.get('value');
       resultValue.displayedValue = this.get('displayedValue');
-      topic.publish(e,resultValue);
+      topic.publish(e, resultValue);
     },
 
-    _emitChange: function() {
-      this._emit(this.id+"onChange");
-    },   
+    _emitChange: function () {
+      this._emit(this.id + "onChange");
+    },
 
-    constructor: function(args) {
+    constructor: function (args) {
       this.inherited(arguments);
       dojo.safeMixin(this, args);
     },
 
-    postCreate: function() {
+    postCreate: function () {
       this.inherited(arguments);
       this.oldVal = this.get('value');
       aspect.after(this, "onChange", lang.hitch(this, "_emitChange"));
     },
 
     // HARD reset with a re-query
-    requery: function(query) {
+    requery: function (query) {
       this.query = query;
       this.removeOption(this.getOptions());
       this.setStore(this.store);
+      this.set('value', this.get('value'));
     },
 
-    startup: function() {
+    startup: function () {
       this.inherited(arguments);
     }
-
     // need to impliment unsubscribes to destroy methods
-
-
-    
-
   });
 });
