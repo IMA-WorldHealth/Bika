a6fa08912ce99337a706474530a2bfb660fbbd70
Steven Fountain <StevenFountain@live.co.uk>
2013-10-03 16:33:42 +0100

    Indentation styling, tabs to spaces (size: 2). Application fiscal year select and styling

--- public/js/dojoos/customizedModule/TabManager.js
+++ public/js/dojoos/customizedModule/TabManager.js
@@ -1,113 +1,113 @@
 define("customizedModule/TabManager", 
-	["dijit/layout/TabContainer", "dojox/layout/ContentPane","dojo/_base/declare"], 
-	function(TabContainer, ContentPane, declare){
-		return declare("customizedModule.TabManager",TabContainer,{
+  ["dijit/layout/TabContainer", "dojox/layout/ContentPane","dojo/_base/declare"], 
+  function(TabContainer, ContentPane, declare){
+    return declare("customizedModule.TabManager",TabContainer,{
 
-			selectedTab:null,
-			constructor:function(args){				
-				this.watch("selectedChildWidget", function(name, oval, nval){
-			    selectedTab = nval;
-			    if (this.getChildren().length == 1){
-			    	this.getChildren()[0].set('closable', false);
-			    }
-			    });				
-			},
-			addChildren: function(title, href, closable, simpleOpen) {
+      selectedTab:null,
+      constructor:function(args){       
+        this.watch("selectedChildWidget", function(name, oval, nval){
+          selectedTab = nval;
+          if (this.getChildren().length == 1){
+            this.getChildren()[0].set('closable', false);
+          }
+          });       
+      },
+      addChildren: function(title, href, closable, simpleOpen) {
 
-				var enfants = this.getChildren();
-				if(enfants.length == 0) {
+        var enfants = this.getChildren();
+        if(enfants.length == 0) {
 
-					//le tabcontainer ne contient aucun enfant, donc on ajoute
-					var aine = new ContentPane({
-         						title: title,
-         						href: href,
-         						closable: false
-    				});
-    				this.addChild(aine);
+          //le tabcontainer ne contient aucun enfant, donc on ajoute
+          var aine = new ContentPane({
+                    title: title,
+                    href: href,
+                    closable: false
+            });
 
+            this.addChild(aine);
 
-				}else if(enfants.length == 1){
+        }else if(enfants.length == 1){
 
-					//verification existence du title
-					var existe = false;
-					var indexExistence = -1;
+          //verification existence du title
+          var existe = false;
+          var indexExistence = -1;
 
-					for(i=0; i< enfants.length; i++){
-						if(title==enfants[i].title){
-							existe = true;
-							indexExistence = i;
-							break;
-						}
-					}
-					if(existe == true){
+          for(i=0; i< enfants.length; i++){
+            if(title==enfants[i].title){
+              existe = true;
+              indexExistence = i;
+              break;
+            }
+          }
+          if(existe == true){
 
-						this.selectChild(enfants[indexExistence]);
+            this.selectChild(enfants[indexExistence]);
 
-					}else {
-						//ajout de l'enfant
+          }else {
+            //ajout de l'enfant
 
-							if(simpleOpen){
-								selectedTab.set('title', title);
-								selectedTab.set('href', href);
-								selectedTab.set('closable', false);								
-		    				}else{
-		    					var d = new ContentPane({
-	         						title: title,
-	         						href: href,
-	         						closable: true
-	    						});
-	    						this.addChild(d);
-		    				}				
+              if(simpleOpen){
+                selectedTab.set('title', title);
+                selectedTab.set('href', href);
+                selectedTab.set('closable', false);               
+                }else{
+                  var d = new ContentPane({
+                      title: title,
+                      href: href,
+                      closable: true
+                  });
+                  this.addChild(d);
+                }       
 
-					}
+          }
 
                   if(!simpleOpen){
-                  	enfants[0].set("closable", true);
+                    enfants[0].set("closable", true);
                   }
-				}
-				else if(enfants.length > 1){					
+        }
+        else if(enfants.length > 1){          
 
-					//verification existence du title
-					var existe = false;
-					var indexExistence = -1;
+          //verification existence du title
+          var existe = false;
+          var indexExistence = -1;
 
-					for(i=0; i< enfants.length; i++){
-						if(title==enfants[i].title){
-							existe = true;
-							indexExistence = i;
-							break;
-						}
-					}
-					if(existe == true){
+          for(i=0; i< enfants.length; i++){
+            if(title==enfants[i].title){
+              existe = true;
+              indexExistence = i;
+              break;
+            }
+          }
+          if(existe == true){
 
-						this.selectChild(enfants[indexExistence]);
+            this.selectChild(enfants[indexExistence]);
 
-					}else {
-						//ajout de l'enfant
+          }else {
+            //ajout de l'enfant
 
-							if(simpleOpen){
-								//enfants[0].set('title', title);
-								//enfants[0].set('href', href);
-								selectedTab.set('title', title);
-								selectedTab.set('href', href);
-								selectedTab.set('closable', closable);								
-		    				}else{
-		    					var d = new ContentPane({
-	         						title: title,
-	         						href: href,
-	         						closable: true
-	    						});
-	    						this.addChild(d);
-		    				}
-									
+              if(simpleOpen){
+                //enfants[0].set('title', title);
+                //enfants[0].set('href', href);
+                selectedTab.set('title', title);
+                selectedTab.set('href', href);
+                selectedTab.set('closable', closable);                
+                }else{
+                  var d = new ContentPane({
+                      title: title,
+                      href: href,
+                      closable: true
+                  });
+                  this.addChild(d);
+                }
+                  
 
-					}
+          }
 
-				}
+        }
 
-				
-				
-			}
-		});
-	}
-	);
+        
+        
+      }
+    });
+  }
+  );
