1529e04438f4c9dc59fa375a4c7dc09970dd267a
Steven Fountain <StevenFountain@live.co.uk>
2013-10-04 15:33:04 +0100

    register callbacks with ApplicationState

--- public/js/dojoos/customizedModule/ApplicationState.js
+++ public/js/dojoos/customizedModule/ApplicationState.js
@@ -1,51 +1,58 @@
 define([
-	"dojo/_base/declare",
-	"dojo/on"
+  "dojo/_base/declare",
+  "dojo/on"
 ], function(declare, on) { 
 
-	//////
-	// module: ApplicationState
-	// description: Maintains pointers to top level application components and their attributes (values, events etc.), these values can 
-	// then be safely loaded and used by modules nested in the application
-	// temporary: What purpose does this serve?
-	// -cache value of often accessed componenets - fewer requests to the DOM 
-	// -register callbacks for all modules, only one event listener registered
-	// -handles removal of callbacks after a module is removed (something we haven't figured out how to do from inside the module yet)
-	//////
-	
-	var component = {};
-	var listener = {};
+  //////
+  // module: ApplicationState
+  // description: Maintains pointers to top level application components and their attributes (values, events etc.), these values can 
+  // then be safely loaded and used by modules nested in the application
+  // TODO: 
+  //  -Doesn't currently support multiple event types - I'm not losing any sleep over it
+  // temporary: What purpose does this serve?
+  // -cache value of often accessed componenets - fewer requests to the DOM 
+  // -register callbacks for all modules, only one event listener registered
+  // -handles removal of callbacks after a module is removed (something we haven't figured out how to do from inside the module yet)
+  //////
+  
+  var component = {};
+  var listener = {};
 
-	//Allows capplication track and remove event handles to respond to errors etc. not used otherwise
-	var handle = {};
+  //Allows capplication track and remove event handles to respond to errors etc. not used otherwise
+  var handle = {};
 
-	var setComponent = function(component_id, reference) { 
-		component[component_id] = reference;
-		handle[component_id] = on(component[component_id], "change", loopCallback(component_id));
-		listener[component_id] = {};
-	}
+  var setComponent = function(component_id, reference) { 
+    component[component_id] = reference;
+    handle[component_id] = on(component[component_id], "change", loopCallback(component_id));
+    listener[component_id] = {};
+  }
 
-	var getComponent = function(component_id) { 
-		return component[component_id];
-	}
+  var getComponent = function(component_id) { 
+    return component[component_id];
+  }
 
-	var register = function(caller_id, component_id, callback) { 
-		//good lawd
-		listener[component_id][caller_id] = callback;
-	}
+  //return true on success, false on failure - don't have to do this
+  var register = function(caller_id, component_id, callback) { 
+    var r = listener[component_id][caller_id];
+    if(!r) { 
+      listener[component_id][caller_id] = callback;
+    }
+  }
 
-	//@sfount - raw javascript haX (ref: Javascript Closure)
-	var loopCallback = function(component_id) { 
-		return function(value) { 
-			console.log(listener);
-		}
-	}
+  //@sfount - raw javascript haX (ref: Javascript Closure)
+  var loopCallback = function(component_id) { 
+    return function(value) { 
+      for(caller in listener[component_id]) { 
+        listener[component_id][caller](value);
+      }
+    }
+  }
 
-	return declare("customizedModule.ApplicationState", null, { 
-		//summary: returns an object to be used by a nested module requiring access to top level application components, this object 
-		//should expose read-only access to values
-		getComponent : getComponent,
-		setComponent : setComponent,
-		register : register
-	});
+  return declare("customizedModule.ApplicationState", null, { 
+    //summary: returns an object to be used by a nested module requiring access to top level application components, this object 
+    //should expose read-only access to values
+    getComponent : getComponent,
+    setComponent : setComponent,
+    register : register
+  });
 });
